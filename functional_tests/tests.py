af7c22de5fdecdf8b55483718dead1b48196d469
wuqin <1509802731@qq.com>
2018-04-03 22:59:32 +0800

    URl for user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,10 +39,11 @@ class NewVisitorTest(LiveServerTestCase):
         #伊迪斯的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        #她按回车键后，页面更新了
-        #待办事件表格中显示了“1.Buy peacock feathers”
+        #她按回车键后，被带到了一个新URL
+        #待办事件表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         
         #页面中又显示了一个文本框，可以输入其他事项
@@ -55,6 +56,34 @@ class NewVisitorTest(LiveServerTestCase):
         #页面再次更新，请单中显示了这两个待办事项
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1:Buy peacock feathers')
+
+        #现在一个叫弗朗西斯的新用户访问了网站
+        ##我们使用一个新浏览器会话
+        ##确保伊迪斯的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #弗朗西斯访问首页
+        #页面中看不到伊迪斯的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+
+        #弗朗西斯输入新的待办事件
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Do homework')
+        inputbox.send_keys(Keys.ENTER)
+
+        #弗朗西斯获得了他的唯一url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('Do homework',page_text)
+        
 
         #伊迪斯想知道这个网站是否会记住她的清单
         #她看到网站为她生成了一个唯一的URL
