04335cff2c115659e8f8f639f61f0adfcc8aa4d8
wuqin <1509802731@qq.com>
2018-04-04 20:40:08 +0800

    URL,refactoring,simplify views

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,10 @@ from lists.models import Item
 # Create your views here.
 #在这儿编写视图
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
