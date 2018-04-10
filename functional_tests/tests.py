5f89f028a56bf2ff03bb0ba82dc9468623c40758
wuqin <1509802731@qq.com>
2018-04-10 23:08:59 +0800

    new URL + view for adding to existing lists.FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
 
         #她注意到网页的标题和头部都包含“TO-DO”这个词
-        time.sleep(5)
+        time.sleep(1)
         self.assertIn("To-Do",self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
@@ -44,18 +44,20 @@ class NewVisitorTest(LiveServerTestCase):
         #她按回车键后，被带到了一个新URL
         #待办事件表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(3)
         edith_list_url = self.browser.current_url
-        time.sleep(5)
+        time.sleep(1)
         self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         
         #页面中又显示了一个文本框，可以输入其他事项
         #她输入了“Use peacock feathers to make a fly”(使用孔雀羽毛做假蝇)
         #伊迪斯做事很有条理
+        time.sleep(2)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #页面再次更新，请单中显示了这两个待办事项
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
@@ -80,18 +82,13 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         #弗朗西斯获得了他的唯一url
-        time.sleep(5)
+        time.sleep(1)
         francis_list_url = self.browser.current_url
-        time.sleep(5)
+        time.sleep(1)
         self.assertRegex(francis_list_url,'/lists/.+')
         self.assertNotEqual(francis_list_url,edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertIn('Do homework',page_text)
-        
 
-        self.fail("Finish the test!")
-
-        #她访问那个URL，发现她的待办事件清单还在
-        #她很满意，去睡觉了
