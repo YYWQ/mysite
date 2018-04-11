fb550be940797c587f1e6ea5fd9df7fded2ef250
wuqin <1509802731@qq.com>
2018-04-11 13:25:51 +0800

    first part and a little changes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -92,3 +92,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertIn('Do homework',page_text)
 
+    def test_layout_and_styling(self):
+        #伊迪斯访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #她看到输入框完美的居中展示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+                               512,delta=6.5)
+        time.sleep(1)
+        #她新建了一个清单，看到输入框仍完美的居中显示
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+                               512,delta=6.5)
+        
+
