afb9a4ec56fadc7317c4fd08afbfc864a6565c74
wuqin <1509802731@qq.com>
2018-05-17 20:58:10 +0800

    view tests finished!

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,7 +2,7 @@ from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 import time
 class ItemValidationTest(FunctionalTest):
-    
+    #验证空的待办事项
     def test_cannot_add_empty_list_items(self):
         #伊迪斯访问首页时，不小心提交了一个空的待办事项
         #输入框没输入内容，她就按下了回车键
@@ -19,7 +19,7 @@ class ItemValidationTest(FunctionalTest):
 
         #她输入一些文字，然后再次提交，这次没问题了
         inputbox = self.get_item_input_box()
-        inputbox.send_keys('Buy milk\n')
+        inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1:Buy milk')
 
@@ -35,9 +35,28 @@ class ItemValidationTest(FunctionalTest):
         
         #输入文字后就没问题了
         inputbox = self.get_item_input_box()
-        inputbox.send_keys('Make tea\n')
+        inputbox.send_keys('Make tea')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
         self.check_for_row_in_list_table('1:Buy milk')
         self.check_for_row_in_list_table('2:Make tea')
+
+    #验证重复待办事项
+    def test_cannot_add_duplicate_items(self):
+        #伊迪斯访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy bananas')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy bananas')
+
+        #她不小心输入了一个重复的待办事项
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy bananas')
+        inputbox.send_keys(Keys.ENTER)
+
+        #她看到一条有帮助的错误信息
+        self.check_for_row_in_list_table('1:Buy bananas')
+        error = self.browser.find_element_by_class_name("help-block")
+        self.assertEqual(error.text,"You've already got this in your list")
         
--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import redirect,render
 from django.http import HttpResponse
 from django.contrib import auth
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm,ItemForm
 from lists.models import Item,List
 
 # Create your views here.
@@ -33,12 +33,13 @@ def home_page(request):
 
 def view_list(request,list_id):
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_,data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
-            return redirect(list_)
+            form.save()
+            #Item.objects.create(text=request.POST['text'], list=list_)
+            #return redirect(list_)
     return render(request,'list.html',{'list':list_, 'form': form})
 
 
