5b9012b719cb40aa5282e3ff32ecee4a3fe1ab7c
wuqin <1509802731@qq.com>
2018-05-17 16:05:56 +0800

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -4,8 +4,9 @@ from lists.models import Item,List
 
 # Create your tests here.
 
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
 
+    '''这个测试被分成以下两个测试方法了
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
@@ -31,7 +32,17 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text,'Item the second')
-        self.assertEqual(second_saved_item.list,list_)
+        self.assertEqual(second_saved_item.list,list_)'''
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text,'')
+
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item,list_.item_set.all())
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -40,6 +51,25 @@ class ListAndItemModelTest(TestCase):
             item.save()
             item.full_clean()
 
+class ListModelTest(TestCase):
+    
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+    #检查同一个清单中有重复的待办事件时是否抛出异常
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_,text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+            
+
+    #确保完整性约束不要做过头了
+    def test_CAN_save_same_item_to_diffrent_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1,text='bla')
+        item = Item(list=list2,text='bla')
+        item.full_clean() #不该抛出异常
